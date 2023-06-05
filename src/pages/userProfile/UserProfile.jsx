2e73cc233114c85696f32644af253bbc676f6fed
Slavik <theslavprod@gmail.com>
2023-06-05 18:55:22 +0400

    first commit

---
+++ src/pages/userProfile/UserProfile.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import styled from "styled-components"
+import { SettingsBlock } from "../../components/settingsBlock/SettingsBlock"
+import { get_all_folder } from "../../store/action/action"
+import { FoldersBlock } from "../foldersBlock/FoldersBlock"
+
+export const UserProfile = () => {
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        dispatch(get_all_folder())
+    },[])
+    return (
+        <>
+        <SettingsBlock/>
+        {/* <Title>Папки</Title> */}
+        <FoldersBlock/>
+        </>
+    )
+}
+const Title = styled.p `
+    text-align: left;
+    max-width: 1170px;
+    width: 95%;
+    margin: auto;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 30px;
+    line-height: 35px;
+    color: #333333;
+    margin: 20px auto;
+`
---
+++ src/store/reducer/Reducer.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux'
+import { addPhotoReducer } from './addPhotoReducer'
+import { changeDataReducer } from './changeDataReducer'
+import { createFolderReducer } from './createFolderReducer'
+import { forgotPassword } from './forgotPasswordReducer'
+import { loginReducer } from './LoginReducer'
+import { registerRegister } from './RegisterReducer'
+export default combineReducers({
+    reg:registerRegister,
+    login:loginReducer,
+    forgotPassword:forgotPassword,
+    changeData:changeDataReducer,
+    creatFolder:createFolderReducer,
+    addPhoto:addPhotoReducer
+})
